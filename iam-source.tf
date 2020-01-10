531b13c83fe880e3e36bd9e7d190379dbd636827
Falpangaea <cas.thomas.r@gmail.com>
2020-01-10 15:23:42 -0500

    remove aws.source line

--- iam-source.tf
+++ iam-source.tf
@@ -21,24 +21,20 @@ data "aws_iam_policy_document" "user_policy" {
 }
 
 resource "aws_iam_policy" "user_policy" {
-  provider   = "aws.source"
   name_prefix = "${var.user_name}-"
   policy      = "${data.aws_iam_policy_document.user_policy.json}"
 }
 
 resource "aws_iam_user" "user" {
-  provider   = "aws.source"
   name          = "${var.user_name}"
   force_destroy = "${var.force_destroy}"
 }
 
 resource "aws_iam_user_policy_attachment" "user_policy" {
-  provider   = "aws.source"
   user       = "${aws_iam_user.user.name}"
   policy_arn = "${aws_iam_policy.user_policy.arn}"
 }
 
 resource "aws_iam_access_key" "key" {
-  provider   = "aws.source"
   user     = "${aws_iam_user.user.name}"
 }
