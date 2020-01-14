abfbc3bd36a86b55b0bffcff2c9b9eb79c453a54
Falpangaea <cas.thomas.r@gmail.com>
2020-01-14 15:07:37 -0500

    changed local parsing:

--- locals.tf
+++ locals.tf
@@ -1,10 +1,7 @@
 locals {
   "bucket_arn"               = "arn:aws:s3:::${var.bucket_name}"
   "object_arn"               = "arn:aws:s3:::${var.bucket_name}/${var.prefix}*"
-  policy                     = "${var.write_access ? "${var.delete_access ?
-                                    data.aws_iam_policy_document.user_policy_read_write_delete.json :
-                                    data.aws_iam_policy_document.user_policy_read_write.json
-                                  }" :
-                                  data.aws_iam_policy_document.user_policy_read.json
-                                }"
+  write_access               = "${var.write_access ? "_write" : ""}"
+  delete_access              = "${var.delete_access ? "_delete" : ""}"
+  policy                    = "${data.aws_iam_policy_document.user_policy_read${local.write_access}${local.delete_access}.json}"
 }
