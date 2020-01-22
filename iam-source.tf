9a145799fef946bf689897bfa79d708b889e06e4
Falpangaea <cas.thomas.r@gmail.com>
2020-01-22 13:42:06 -0500

    upgrade to 0.12.17

--- iam-source.tf
+++ iam-source.tf
@@ -1,11 +1,44 @@
+data "aws_iam_user" "user" {
+  count     = var.create_user ? 0 : 1
+  user_name = var.user_name
+}
+
+resource "aws_iam_policy" "user_policy" {
+  name_prefix = "${var.user_name}-"
+  policy      = local.policy
+}
+
+resource "aws_iam_user" "user" {
+  count         = var.create_user ? 1 : 0
+  name          = var.user_name
+  force_destroy = var.force_destroy
+}
+
+resource "aws_iam_user_policy_attachment" "user_policy" {
+  user       = aws_iam_user.user[0].name
+  policy_arn = aws_iam_policy.user_policy.arn
+}
+
+resource "aws_iam_access_key" "key" {
+  user = aws_iam_user.user[0].name
+}
+
 data "aws_iam_policy_document" "user_policy_read" {
   statement {
     actions = [
       "s3:ListBucket",
     ]
 
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibility in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
     resources = [
-      "${local.bucket_arn}",
+      local.bucket_arn,
     ]
 
     condition {
@@ -25,8 +58,16 @@ data "aws_iam_policy_document" "user_policy_read_write" {
       "s3:PutObject",
     ]
 
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibility in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
     resources = [
-      "${local.object_arn}",
+      local.object_arn,
     ]
   }
 
@@ -35,8 +76,16 @@ data "aws_iam_policy_document" "user_policy_read_write" {
       "s3:ListBucket",
     ]
 
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibility in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
     resources = [
-      "${local.bucket_arn}",
+      local.bucket_arn,
     ]
 
     condition {
@@ -54,11 +103,19 @@ data "aws_iam_policy_document" "user_policy_read_write_delete" {
   statement {
     actions = [
       "s3:PutObject",
-      "s3:DeleteObject"
+      "s3:DeleteObject",
     ]
 
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibility in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
     resources = [
-      "${local.object_arn}",
+      local.object_arn,
     ]
   }
 
@@ -67,8 +124,16 @@ data "aws_iam_policy_document" "user_policy_read_write_delete" {
       "s3:ListBucket",
     ]
 
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibility in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
     resources = [
-      "${local.bucket_arn}",
+      local.bucket_arn,
     ]
 
     condition {
@@ -82,21 +147,3 @@ data "aws_iam_policy_document" "user_policy_read_write_delete" {
   }
 }
 
-resource "aws_iam_policy" "user_policy" {
-  name_prefix = "${var.user_name}-"
-  policy      = "${local.policy}"
-}
-
-resource "aws_iam_user" "user" {
-  name          = "${var.user_name}"
-  force_destroy = "${var.force_destroy}"
-}
-
-resource "aws_iam_user_policy_attachment" "user_policy" {
-  user       = "${aws_iam_user.user.name}"
-  policy_arn = "${aws_iam_policy.user_policy.arn}"
-}
-
-resource "aws_iam_access_key" "key" {
-  user     = "${aws_iam_user.user.name}"
-}
--- variables.tf
+++ variables.tf
@@ -1,16 +1,21 @@
 variable "bucket_name" {
-  type        = "string"
+  type        = string
   description = "Name for s3 bucket"
 }
 
 variable "prefix" {
-  type        = "string"
+  type        = string
   description = "Prefix to grant access to, note this module does not add any trailing /"
 }
 
 variable "user_name" {
-  type        = "string"
+  type        = string
   description = "Short name for the IAM user to create"
+}
+
+variable "create_user" {
+  default     = true
+  description = "True if a user needs to be created.  False if the user already exists."
 }
 
 variable "force_destroy" {
